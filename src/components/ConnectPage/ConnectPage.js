2e14362eee55fae3748a69acffb3e70dcfa582b6
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-25 14:27:17 +0800

    layout

--- src/components/ConnectPage/ConnectPage.js
+++ src/components/ConnectPage/ConnectPage.js
@@ -1,8 +1,9 @@
 /*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
 
-import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react/addons';
 import styles from './ConnectPage.less';
 import withStyles from '../../decorators/withStyles';
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 @withStyles(styles)
 class ConnectPage extends React.Component {
@@ -10,14 +11,15 @@ class ConnectPage extends React.Component {
     super(props);
 
     this.state = {
-      range: ['food', 'vietnamese'],
-      cursor: 'eating',
+      range: [],
       category: {
-        eating: ['food', 'bad-food', 'poison', 'vietnamese'],
-        gaming: ['game 1', 'game 2', 'game 3'],
-        sleeping: ['nap', 'daydraming', 'good night sleep']
+        'comfort & support': ['relationships', 'secrets', 'works', 'parenting', 'health', 'great losses', 'homesick', 'shame', 'bad moods', 'sleepless nights'],
+        'life styles': ['cooking', 'fitness', 'interior design', 'DIY', 'fashion', 'makeups', 'traveling', 'hiking', 'sports', 'dancing', 'new hobbies'],
+        'art & popular culture': ['novels', 'poets', 'movies', 'anime', 'pop music', 'classical music', 'conserts', 'games', 'comnic', 'comedy'],
+        'learning': ['dinosaurs', 'wild lifes', 'history', 'foreign culture', 'literature', 'languages', 'physics', 'mathematics', 'chemistry', 'technology']
       }
     };
+    this.state.cursor = Object.keys(this.state.category)[0];
   }
 
   static contextTypes = {
@@ -37,11 +39,9 @@ class ConnectPage extends React.Component {
   onSelect(interest) {
     let index = this.state.range.indexOf(interest);
     if (index == -1) {
-      let array = this.state.range.slice();
-      array.push(interest);
-      this.setState({
-        range: array
-      });
+      let range = this.state.range;
+      range.push(interest);
+      this.setState({range: range});
     }
   }
 
@@ -61,20 +61,41 @@ class ConnectPage extends React.Component {
     //this.context.onSetTitle(title);
     return (
       <div className="ConnectPage">
-        <div className="ConnectPage-container">
-          <div className="ConnectPage-range">
-            <h3 className="text-center">Connect through your interests</h3>
-            <ul className="ConnectPage-range-list">
-            {this.state.range.map(function(data, i) {
-              return (
-                <li className="ConnectPage-range-item"
-                  onClick={this.onDeselect.bind(this, data)}>
-                  <i className="fa fa-times" />
-                  {data}
-                </li>
-              );
-            }.bind(this))}
-            </ul>
+        <div className="ConnectPage-range">
+          <div className="ConnectPage-container">
+            Let&apos;s&nbsp;
+            <span className="ConnectPage-range-item">
+              <i className="fa fa-play" />
+              talk
+            </span>
+            &nbsp;to people who also get&nbsp;
+            {function() {
+              if (this.state.range.length == 0) {
+                return (
+                  <span className="ConnectPage-range-item">
+                    <i className="fa fa-spinner fa-pulse" />
+                    something
+                  </span>
+                );
+              } else {
+                return (
+                  <ul className="ConnectPage-range-list">
+                  {this.state.range.map(function(data, i) {
+                    return (
+                      <ReactCSSTransitionGroup transitionName="carousel">
+                        <li className="ConnectPage-range-item"
+                          onClick={this.onDeselect.bind(this, data)}>
+                          <i className="fa fa-times" />
+                          {data}
+                        </li>
+                      </ReactCSSTransitionGroup>
+                    );
+                  }.bind(this))}
+                  </ul>
+                );
+              }
+            }.bind(this)()}
+            &nbsp;stuck in their heads.
           </div>
         </div>
         <div className="ConnectPage-container">
@@ -93,12 +114,15 @@ class ConnectPage extends React.Component {
             <ul className="ConnectPage-category-minor-list">
               {this.state.category[this.state.cursor].map(function(data, i) {
                 let className = "ConnectPage-category-minor-item";
-                if (this.state.range.indexOf(data) != -1)
+                let iconName = "fa fa-check";
+                if (this.state.range.indexOf(data) != -1) {
                   className += " ConnectPage-category-minor-item-active";
+                  iconName = "fa fa-circle-o";
+                }
                 return (
                   <li className={className}
                     onClick={this.onSelect.bind(this, data)}>
-                    <i className="fa fa-check" />
+                    <i className={iconName} />
                     {data}
                   </li>
                 );
